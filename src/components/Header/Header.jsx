145269ae694ad609be6d2936cff093c16b166d59
Rithik Sharma <srithik645@gmail.com>
2023-12-17 01:12:54 +0530

    Home Section and service section ready !!!

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,24 @@ import { NavLink } from 'react-router-dom';
 
 import logo from '../../assets/images/eco-logo.png'
 import userIcon from '../../assets/images/user-icon.png'
+import { motion } from 'framer-motion';
 
 import { Container, Row } from 'reactstrap';
+
+const nav__links = [
+    {
+        path: 'home',
+        display: 'Home'
+    },
+    {
+        path: 'shop',
+        display: 'Shop'
+    },
+    {
+        path: 'cart',
+        display: 'Cart'
+    },
+]
 
 const Header = () => {
     return <header className='header'>
@@ -16,28 +32,35 @@ const Header = () => {
                         <img src={logo} alt="logo" />
                         <div>
                             <h1>Multimart</h1>
-                            <p>Since 1995</p>
+                            {/* <p>Since 1995</p> */}
                         </div>
                     </div>
 
                     <div className="naigation">
                         <ul className="menu">
-                            <li className="nav__item">
-                                <NavLink to='home'>Home</NavLink>
-                            </li>
-                            <li className="nav__item">
-                                <NavLink to='home'>Shop</NavLink>
-                            </li>
-                            <li className="nav__item">
-                                <NavLink to='home'>Cart</NavLink>
-                            </li>
+                            {
+                                nav__links.map((item, index) => (
+                                    <li className="nav__item" key={index}>
+                                        <NavLink to={item.path} className={(navClass) =>
+                                            navClass.isActive ? 'nav__active' : ''}>{item.display}</NavLink>
+                                    </li>
+                                ))
+                            }
                         </ul>
                     </div>
 
                     <div className="nav__icons">
-                        <span className='fav__icon'><i class="ri-heart-line"></i></span>
-                        <span className="cart__icon"><i class="ri-shopping-bag-line"></i></span>
-                        <span><img src={userIcon} alt="" /></span>
+                        <span className='fav__icon'>
+                            <i class="ri-heart-line"></i>
+                            <span className="badge">1</span>
+                        </span>
+                        <span className="cart__icon">
+                            <i class="ri-shopping-bag-line"></i>
+                            <span className="badge">1</span>
+                        </span>
+                        <span>
+                            <motion.img whileTap={{ scale: 1.2 }} src={userIcon} alt="" />
+                        </span>
                     </div>
 
                     <div className="mobile__menu">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import Helmet from "../components/Helmet/Helmet";
+import "../styles/home.css";
+import { Col, Container, Row } from "reactstrap";
+import Services from "../services/Services";
+import heroImg from "../assets/images/hero-img.png";
 
 const Home = () => {
-    return (
-        <div>Home</div>
-    )
-}
+  const year = new Date().getFullYear();
+  return (
+    <Helmet title={"Home"}>
+      <section className="hero__section">
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div className="hero__content">
+                <p className="hero__subtitle">Trending product in {year}</p>
+                <h2>Make Your Interior More Minimalistic & Modern</h2>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
+                  excepturi blanditiis magni id molestiae cum! Modi corrupti
+                  eius architecto est.
+                </p>
 
-export default Home
+                <motion.button whileTap={{ scale: 1.2 }} className="buy__btn">
+                  <Link to="/shop"> SHOP NOW</Link>
+                </motion.button>
+              </div>
+            </Col>
+
+            <Col lg="6" md="6">
+              <div className="hero__img">
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <Services />
+    </Helmet>
+  );
+};
+
+export default Home;
