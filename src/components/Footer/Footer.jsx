a291312272216d18d81089cc5f1786f5ca964eff
Rithik Sharma <srithik645@gmail.com>
2023-12-17 16:25:31 +0530

    Home page complete !!!

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,115 @@
-import React from 'react'
+import React from "react";
+import "./footer.css";
+
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import { Link } from "react-router-dom";
+
+import logo from "../../assets/images/eco-logo.png";
 
 const Footer = () => {
-    return (
-        <div>Footer</div>
-    )
-}
+  const year = new Date().getFullYear();
 
-export default Footer
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="4">
+            <div className="logo">
+              <img src={logo} alt="logo" />
+              <div>
+                <h1 className="text-white">Multimart</h1>
+              </div>
+            </div>
+            <p className="footer__text mt-4">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam
+              libero corrupti voluptas culpa vitae commodi sint facilis
+              molestias ipsa vero.
+            </p>
+          </Col>
+
+          <Col lg="3">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Top Categories</h4>
+              <ListGroup>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Mobile Phones</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Modern Sofa</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Arm Chair</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Smart Watches</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="2">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Useful Links</h4>
+              <ListGroup>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/shop">Shop</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/cart">Cart</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/login">Login</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Privacy Policy</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="3">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Contact</h4>
+              <ListGroup className="footer__contact">
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-map-pin-line"></i>
+                  </span>
+                  <p>53/6a IRI Colony, Roorkee (Uttarakhand)</p>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-phone-line"></i>
+                  </span>
+                  <p>+91 9027540995</p>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-mail-line"></i>
+                  </span>
+                  <p>srithik645@gmail.com</p>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="12">
+            <p className="footer__copyright">
+              Copyright {year} developer by Rithik Sharma. All rights reserved.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,77 +1,107 @@
-import React from 'react'
-import './header.css';
-import { NavLink } from 'react-router-dom';
+import React, { useRef, useEffect } from "react";
+import "./header.css";
+import { NavLink } from "react-router-dom";
 
-import logo from '../../assets/images/eco-logo.png'
-import userIcon from '../../assets/images/user-icon.png'
-import { motion } from 'framer-motion';
+import logo from "../../assets/images/eco-logo.png";
+import userIcon from "../../assets/images/user-icon.png";
+import { motion } from "framer-motion";
 
-import { Container, Row } from 'reactstrap';
+import { Container, Row } from "reactstrap";
 
 const nav__links = [
-    {
-        path: 'home',
-        display: 'Home'
-    },
-    {
-        path: 'shop',
-        display: 'Shop'
-    },
-    {
-        path: 'cart',
-        display: 'Cart'
-    },
-]
+  {
+    path: "home",
+    display: "Home",
+  },
+  {
+    path: "shop",
+    display: "Shop",
+  },
+  {
+    path: "cart",
+    display: "Cart",
+  },
+];
 
 const Header = () => {
-    return <header className='header'>
-        <Container>
-            <Row>
-                <div className="nav__wrapper">
-                    <div className="logo">
-                        <img src={logo} alt="logo" />
-                        <div>
-                            <h1>Multimart</h1>
-                            {/* <p>Since 1995</p> */}
-                        </div>
-                    </div>
+  const headerRef = useRef(null);
 
-                    <div className="naigation">
-                        <ul className="menu">
-                            {
-                                nav__links.map((item, index) => (
-                                    <li className="nav__item" key={index}>
-                                        <NavLink to={item.path} className={(navClass) =>
-                                            navClass.isActive ? 'nav__active' : ''}>{item.display}</NavLink>
-                                    </li>
-                                ))
-                            }
-                        </ul>
-                    </div>
+  const menuRef = useRef(null);
 
-                    <div className="nav__icons">
-                        <span className='fav__icon'>
-                            <i class="ri-heart-line"></i>
-                            <span className="badge">1</span>
-                        </span>
-                        <span className="cart__icon">
-                            <i class="ri-shopping-bag-line"></i>
-                            <span className="badge">1</span>
-                        </span>
-                        <span>
-                            <motion.img whileTap={{ scale: 1.2 }} src={userIcon} alt="" />
-                        </span>
-                    </div>
+  const stickyheaderFunc = () => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("sticky__header");
+      } else {
+        headerRef.current.classList.remove("sticky__header");
+      }
+    });
+  };
 
-                    <div className="mobile__menu">
-                        <span>
-                            <i class="ri-menu-fill"></i>
-                        </span>
-                    </div>
-                </div>
-            </Row>
-        </Container>
+  useEffect(() => {
+    stickyheaderFunc();
+
+    return () => window.removeEventListener("scroll", stickyheaderFunc);
+  });
+
+  const menuToggle = () => menuRef.current.classList.toggle("active__menu");
+
+  return (
+    <header className="header" ref={headerRef}>
+      <Container>
+        <Row>
+          <div className="nav__wrapper">
+            <div className="logo">
+              <img src={logo} alt="logo" />
+              <div>
+                <h1>Multimart</h1>
+                {/* <p>Since 1995</p> */}
+              </div>
+            </div>
+
+            <div className="navigation" ref={menuRef} onClick={menuToggle}>
+              <ul className="menu">
+                {nav__links.map((item, index) => (
+                  <li className="nav__item" key={index}>
+                    <NavLink
+                      to={item.path}
+                      className={(navClass) =>
+                        navClass.isActive ? "nav__active" : ""
+                      }
+                    >
+                      {item.display}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="nav__icons">
+              <span className="fav__icon">
+                <i class="ri-heart-line"></i>
+                <span className="badge">1</span>
+              </span>
+              <span className="cart__icon">
+                <i class="ri-shopping-bag-line"></i>
+                <span className="badge">1</span>
+              </span>
+              <span>
+                <motion.img whileTap={{ scale: 1.2 }} src={userIcon} alt="" />
+              </span>
+              <div className="mobile__menu">
+                <span onClick={menuToggle}>
+                  <i class="ri-menu-fill"></i>
+                </span>
+              </div>
+            </div>
+          </div>
+        </Row>
+      </Container>
     </header>
-}
+  );
+};
 
-export default Header
+export default Header;
